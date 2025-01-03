03dcc3b655ad46eb3c6423534e2c866c0c986db2
PacNPal <183241239+pacnpal@users.noreply.github.com>
2025-01-03 15:35:07 -0500

    Initial commit

--- source/usr/local/emhttp/plugins/text-editor/event/dynamix_context_menu.php
+++ source/usr/local/emhttp/plugins/text-editor/event/dynamix_context_menu.php
@@ -8,35 +8,12 @@ $event = str_replace('dynamix.', '', $event);
 
 switch ($event) {
     case 'file.menu':
-        // Only add menu item for files, not directories
+        // Add menu item for all files
         if (isset($type) && $type === 'f') {
-            // Check if it's a text file
-            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            $mime_type = finfo_file($finfo, $path);
-            finfo_close($finfo);
-            
-            $extension = strtolower(pathinfo($path, PATHINFO_EXTENSION));
-            $is_text = strpos($mime_type, 'text/') === 0 || 
-                      in_array($extension, ['txt', 'log', 'cfg', 'conf', 'ini', 'sh', 'php', 'css', 'js', 'xml', 'html']);
-            
-            if ($is_text) {
-                $menu['Edit in Text Editor'] = [
-                    'icon' => 'icon-edit',
-                    'href' => '/Tools/TextEditor?file='.urlencode($path)
-                ];
-                
-                if (isset($menu['Edit'])) {
-                    // Move our entry after the default edit option
-                    $edit = $menu['Edit'];
-                    unset($menu['Edit']);
-                    $menu = array_merge(
-                        array_slice($menu, 0, 1),
-                        ['Edit' => $edit],
-                        ['Edit in Text Editor' => $menu['Edit in Text Editor']],
-                        array_slice($menu, 1)
-                    );
-                }
-            }
+            $menu['Edit in Text Editor'] = [
+                'icon' => 'icon-edit',
+                'href' => '/Tools/TextEditor?file='.urlencode($path)
+            ];
         }
         break;
 }
